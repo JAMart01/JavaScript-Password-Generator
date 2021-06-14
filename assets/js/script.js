39c59d484bcc382f8c3060be5ac118a9caecdab0
JAMart <jorge.martinez1597@gmail.com>
2021-06-13 22:05:20 -0400

    fully functional web application, but it has some bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,20 +23,38 @@
 // Assignment code here
 
 
+// This leads to the button on the page 
+var generateBtn = document.querySelector("#generate");
+
+
+// Arrays that hold all of our possible characters
+var lowercase = ["abcdefghijklmnopqrstuvwxyz"];
+
+var uppercase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+
+var numbers = ["1234567890"];
+
+var special = ["@!#$^%&*"];
+
+
+//function that is going to generate the password 
 var generatePassword = function() {
 
   // Takes user input to determine how long the password should be
   var passwordLength = prompt("How many characters long should your password be?");
   
+  // Added as a precautionary measure to ensure the users input is an whole number 
   passwordLength = parseInt(passwordLength);
 
+
+  // checks to make sure the password is no less than 8 character and no more than 128
   if (passwordLength < 8 || !passwordLength) {
     alert("Your password must be at least 8 characters long. Please enter a valid password length");
-    generatePassword();
+    writePassword();
   }
   else if (passwordLength > 128) {
     alert("Your password should not be longer than 128 characters. Please enter a valid password length.");
-    generatePassword();
+    writePassword();
   }
 
   // Takes users input to determine what type of characters to include
@@ -45,28 +63,39 @@ var generatePassword = function() {
   var confirmNumbers = confirm("Should your password include numeric characters?");
   var confirmSpecial = confirm("Should your password include special characters?");
   
+  // If statement to confirm that at least one character type is chosen if not then it takes the user back to the beggining
   if (!confirmUppercase && !confirmLowercase && !confirmNumbers && !confirmSpecial) {
     alert("You did not choose at least one character type for your password. Please try again.")
-    generatePassword();
+    writePassword();
   }
-  // else if (!confirmUppercase && confirmLowercase && !confirmNumbers && !confirmSpecial) {
-  //   charset = "abcdefghijklmnopqrstuvwxyz"
-  // }
-  // else if (confirmUppercase && !confirmLowercase && !confirmNumbers && !confirmSpecial) {
-  //   charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  // }
-  // else if (!confirmUppercase && !confirmLowercase && confirmNumbers && !confirmSpecial) {
-  //   charset = "123456789"
-  // }
-  // else if (!confirmUppercase && !confirmLowercase && !confirmNumbers && confirmSpecial) {
-  //   charset = "@!#$^%&*"
-  // }
-  
+ 
+  // if statements to change the value of the arrays based on users preffered parameters
+  if (!confirmLowercase) {
+    lowercase = []
+  }
+  if (!confirmUppercase) {
+    uppercase = []
+  }
+  if (!confirmNumbers) {
+    numbers = []
+  }
+  if (!confirmSpecial) {
+    special = []
+  }
 
+
+// Separate variable to combine arrays and make it easier to randomize passwords
+var passParameters = lowercase + uppercase + numbers + special
+    // container to hold generated password
+    var completePass = "";
+    for (i= 0; i < passwordLength; i++) {
+      var randomize = passParameters[Math.floor(Math.random() * passParameters.length)];
+      completePass += randomize;
+
+    }   
+    return completePass
 };
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
